9c157f2bda444b71e2b517408772994adeb43442
JDEast1029 <852778359@qq.com>
2017-12-28 16:31:47 +0800

    lucky-wheel

--- ROLLUP_CONFIG.md
+++ ROLLUP_CONFIG.md
@@ -1,6 +1,6 @@
 # rollup 搭配 React并发布到NPM 遇到的问题
 1. `npm install -D babel-plugin-transform-react-jsx` 让rollup可以解析JSX语法
-2. 发布到npm的时候都要创建一个`index.js`文件，并导出自己的组件
+2. 发布到npm的时候为了方便引入，要创建一个`index.js`文件，并导出自己的组件
 ```js
 module.exports = require('./lib/LuckyWheel');
 // 或
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve'; 
 import commonjs from 'rollup-plugin-commonjs';
+import scss from 'rollup-plugin-scss'
 
 const config = {
 	input: './src/main.js',
@@ -11,9 +12,13 @@ const config = {
 	moduleName: 'LuckyWheel',
 	external: [
 		'react',
-		'react-dom'
+		'react-dom',
+		'prop-types'
 	],
 	plugins: [
+		scss({
+			output: 'lib/css/styles.css',
+		}),
 		resolve(),
 		babel({
 			exclude: 'node_modules/**' // 只编译我们的源代码
